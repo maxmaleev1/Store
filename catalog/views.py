e735dfd5d4c4568e73697f0997e0a4e1088e57fc
Максим Малеев <mmaleev1@gmail.com>
2025-03-07 00:40:38 +0300

    HW_24

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
-
-
-def home(request):
-    return render(request, 'home.html')
+from django.views.generic import DetailView, ListView, TemplateView
+from .models import Product
 
 
 def contacts(request):
     return render(request, 'contacts.html')
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'product_detail.html'
+    context_object_name = 'product'
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'product_list.html'
+    context_object_name = 'products'
--- config/settings.py
+++ config/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-DEBUG = True if os.getenv("SECRET_KEY") == "True" else False
+DEBUG = True if os.getenv("DEBUG") == "True" else False
 
 ALLOWED_HOSTS = ['*']
 
@@ -87,8 +87,8 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = (BASE_DIR / 'static',)
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR/"static",]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
