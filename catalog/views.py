aad013829e040f9f8de4b6f0fe3fa8122c2fed98
Максим Малеев <mmaleev1@gmail.com>
2025-03-07 15:08:00 +0300

    HW_24 Удалил указание на стандартные шаблоны(шаблоны переместил в необходимую папку) в классах Детейл и Лист

--- catalog/views.py
+++ catalog/views.py
@@ -9,11 +9,9 @@ def contacts(request):
 
 class ProductDetailView(DetailView):
     model = Product
-    template_name = 'product_detail.html'
     context_object_name = 'product'
 
 
 class ProductListView(ListView):
     model = Product
-    template_name = 'product_list.html'
     context_object_name = 'products'
